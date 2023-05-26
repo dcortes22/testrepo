d2410be1862c878a64e3bdbb088ddd7da22d9b87
dcortes22 <dcortes22@gmail.com>
2023-05-26 14:12:32 -0600

    Adding testing project

---
+++ TestProject/TestProject/TestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectApp.swift
+//  TestProject
+//
+//  Created by David Cortes on 26/5/23.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
